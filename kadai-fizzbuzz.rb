5d071b46e608f9b3b17db0b7f23f48d6d7c7fa5f
taro.kirameki <taro.kirameki@techacademy.jp>
2021-08-05 14:19:21 +0000

    First version

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+num_max = 100
+
+def fizzbuzz(num)   #FizzBuzzメソッドを定義
+  if ((num % 3) == 0) && ((num % 5) == 0)
+    return "FizzBuzz"
+  elsif (num % 3) == 0
+    return "Fizz"
+  elsif (num % 5) == 0
+    return "Buzz"
+  else
+    return num
+  end 
+end
+
+(1..num_max).each do |i|    #ループ
+    puts fizzbuzz(i)
+end
